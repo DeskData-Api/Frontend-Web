fde64dccaab56aec9bebb11cf0cd06fa0de3db2d
Felipe Augusto Graciano <felipe98ju@hotmail.com>
2025-03-16 23:09:06 -0300

    Refactor: Removendo do gittgone o tailwind

---
+++ Frontend-Web/tailwind.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+    content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"], // Certifique-se de que os arquivos corretos estão sendo escaneados
+    theme: {
+      extend: {},
+    },
+    plugins: [],
+  };
+  
